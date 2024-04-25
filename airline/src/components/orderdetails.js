53fe69b9aa29d0e4dfa0ba8dea6c0da7f85e2f50
kc9304 <kkc9304@gmail.com>
2024-04-25 14:20:05 +0530

    payment chnages

--- airline/src/components/orderdetails.js
+++ airline/src/components/orderdetails.js
@@ -6,8 +6,84 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram, faFacebook, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
-
+import Razorpay from 'razorpay';
 const Orderd = () => {
+   
+    const paymentHandler = async (event) => {
+
+        const amount = 500000;
+        const currency = 'INR';
+        const receiptId = '1234567890';
+    
+        const response = await fetch('http://localhost:5000/order', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            amount,
+            currency,
+            receipt: receiptId
+          })
+        })
+    
+          const order = await response.json();
+          console.log('order', order);
+    
+    
+          var option = {
+            key:"",
+            // key_id:"rzp_test_LlSQ8ZbU7Jku2v",
+            amount,
+            currency,
+            name:"KcAirline",
+            description: "Test Transaction",
+            order_id:order.id,
+            handler: async function(response) {
+              
+              const body = {...response,}
+    
+              const validateResponse = await fetch('http://localhost:5000/validate', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(body)
+    
+              })
+    
+              const jsonResponse = await validateResponse.json();
+    
+              console.log('jsonResponse', jsonResponse);
+              
+            },
+            prefill: {
+              name: "Krishna Chaitanya", 
+              email: "krishna@gmail.com",
+              contact: "9000000000", 
+            },
+            notes: {
+              address: "Razorpay Corporate Office",
+            },
+            theme: {
+              color: "orange",
+            },
+          }
+    
+          var rzp1 = new window.Razorpay(option);
+          rzp1.on("payment.failed", function(response) {
+            alert(response.error.code);
+            alert(response.error.description);
+            alert(response.error.source);
+            alert(response.error.step);
+            alert(response.error.reason);
+            alert(response.error.metadata.order_id);
+            alert(response.error.metadata.payment_id);
+          })
+    
+          rzp1.open();
+          event.preventDefault();
+      }
     const navigate = useNavigate();
 
     function scrollTopage() {
@@ -106,7 +182,7 @@ const Orderd = () => {
                                 <input type="checkbox" id="phonePePayment" name="paymentOption" value="phonePe" />
                                 <label style={{ color: "rgb(251, 87, 28)" }} htmlFor="phonePePayment">PhonePe Payment</label>
                             </div>
-                            <button style={{ color: "white", backgroundColor: "rgb(251, 87, 28)", marginTop: "10px" }}>Proceed</button>
+                            <button  onClick={paymentHandler}style={{ color: "white", backgroundColor: "rgb(251, 87, 28)", marginTop: "10px" }}>Proceed</button>
                         </div>
                     </div>
                 </div>
